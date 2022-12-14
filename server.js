8b23d5e0c492a57b2b6411ea9b174672edcc3fc4
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-12-14 11:18:31 +0100

    moving server options to separate file

--- server.js
+++ server.js
@@ -3,29 +3,11 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+const passportConfig = require('./config/passport');
 
 const app = express();
 
-// configure passport provider options
-passport.use(new GoogleStrategy({
-    clientID: '78551848861-07pup6pt8j73h9a5o9nujuupfpdkrgg4.apps.googleusercontent.com',
-    clientSecret: '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',
-    callbackURL: 'http://localhost:8000/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-  done(null, profile);
-}));
-
-// serialize user when saving to session
-passport.serializeUser((user, serialize) => {
-  serialize(null, user);
-});
-
-// deserialize user when reading from session
-passport.deserializeUser((obj, deserialize) => {
-  deserialize(null, obj);
-});
 
 app.use(session({ secret: '3522' }));
 app.use(passport.initialize());
@@ -40,27 +22,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] }));
-
-  app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-    res.redirect('/user/logged');
-    
-  }
-);
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/user', require('./routes/user.routes'));
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
