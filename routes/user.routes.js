76fba7eb956520817548817f62498f24a10b895f
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-12-14 12:30:02 +0100

    Add option log out, page profile and setting

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,34 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  if (req.user) {
+    res.render('logged', {
+      username: req.user.displayName,
+      avatar: req.user.photos[0].value,
+    });
+  } else res.redirect('/user/no-permission');
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+router.get('/profile', (req, res) => {
+  if (req.user) {
+    res.render('profile', {
+      username: req.user.displayName,
+    });
+  } else res.redirect('/user/no-permission');
+});
+
+router.get('/profile/settings', (req, res) => {
+  if (req.user) {
+    res.render('profileSettings');
+  } else res.redirect('/user/no-permission');
+});
+
+router.get('/logout', (req, res) => {
+  res.render('logout')
+  });
 
 module.exports = router;
