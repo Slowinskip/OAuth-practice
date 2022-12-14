ce2410961d2a7f16f3c19aa6656dc70037eba503
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-12-14 10:43:42 +0100

    Add passport to server

--- server.js
+++ server.js
@@ -2,11 +2,38 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 const app = express();
 
+// configure passport provider options
+passport.use(new GoogleStrategy({
+    clientID: '78551848861-07pup6pt8j73h9a5o9nujuupfpdkrgg4.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: 'http://localhost:8000/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  done(null, profile);
+}));
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
+
+app.use(session({ secret: '3522' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
+
 
 app.use(cors());
 app.use(express.json());
